000c2432795e56965e45ec223ca3e5e1fd53ad28
Ilyass El arbaouti <63542705+IlyassElarbaouti@users.noreply.github.com>
2024-03-04 10:44:44 +0100

    Update telegram.js

--- controller/lib/telegram.js
+++ controller/lib/telegram.js
@@ -5,12 +5,13 @@ const MY_TOKEN = "7157319508:AAGlPozeqyyvgq8RXV6A5K5sPDAWopVBWOM"
 var api = new telegram({
     token: MY_TOKEN,
 });
-function sendMessage(body){
-    api.sendPhoto({
+async function sendMessage(body){
+    await api.sendPhoto({
         chat_id:-1002028737966,
         caption: `${body.name}\n\https://learn.dwmaroc.com/courses/${body.slug}`,
          photo: body.image
     })
+    return "success"
 }
 
 
