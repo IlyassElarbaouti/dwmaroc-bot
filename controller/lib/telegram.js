646be9c416c34c517d1059ac1612b739cdc28e80
Ilyass El arbaouti <63542705+IlyassElarbaouti@users.noreply.github.com>
2024-03-07 17:23:36 +0100

    Update telegram.js

--- controller/lib/telegram.js
+++ controller/lib/telegram.js
@@ -21,17 +21,16 @@ async function sendMessage(body = {}) {
 
     // Build the caption dynamically based on the existence of each field
     const caption = [
-        name && `<b>• Name:</b> ${name}`,
-        language && `<b>• Language:</b> ${language}`,
-        courseCondition && `<b>• Course Condition:</b> ${courseCondition}`,
-        category && `<b>• Category:</b> ${category}`,
-        slug && `<b>• Link:</b> <a href="https://learn.dwmaroc.com/courses/${slug}">${slug}</a>`,
+        name && `<b>Name:</b> ${name}`,
+        language && `<b>Language:</b> ${language}`,
+        courseCondition && `<b>Course Condition:</b> ${courseCondition}`,
+        category && `<b>Category:</b> ${category}`,
+        slug && `<b>Link:</b> <a href="https://learn.dwmaroc.com/courses/${slug}">${slug}</a>`,
         "<b>Share our channel to support us:</b>\n@dwmfreecourses",
     ].filter(Boolean).join('\n \n');
 
     await api.sendPhoto({
-        //chat_id: -1001456455807,
-        chat_id: -1002028737966,
+        chat_id: -1001456455807,
         caption,
         photo: image,
         parse_mode: 'HTML', // Enable HTML formatting
