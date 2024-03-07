34104a9a32eb47e8a08d97b8fd5953073b299e91
Ilyass El arbaouti <63542705+IlyassElarbaouti@users.noreply.github.com>
2024-03-07 15:01:53 +0100

    Update telegram.js

--- controller/lib/telegram.js
+++ controller/lib/telegram.js
@@ -5,16 +5,38 @@ const MY_TOKEN = "7157319508:AAGlPozeqyyvgq8RXV6A5K5sPDAWopVBWOM"
 var api = new telegram({
     token: MY_TOKEN,
 });
-async function sendMessage(body){
-    console.log(body)
+
+async function sendMessage(body = {}) {
+    console.log(body);
+
+    // Provide default values for optional fields
+    const {
+        name = '',
+        language = '',
+        courseCondition = '',
+        category = '',
+        slug = '',
+        image = '',
+    } = body;
+
+    // Build the caption dynamically based on the existence of each field
+    const caption = [
+        name && `•name: ${name}`,
+        language && `•language: ${language}`,
+        courseCondition && `•Course condition: ${courseCondition}`,
+        category && `•category: ${category}`,
+        slug && `•link: https://learn.dwmaroc.com/courses/${slug}`,
+        "Share our channel to support us:\n@dwmfreecourses",
+    ].filter(Boolean).join('\n');
+
     await api.sendPhoto({
-        //chat_id:-1001456455807,
-       chat_id: -1002028737966,
-        caption: `•name: ${body.name}\n\•language: ${body.language}\n\•Course contidtion: ${body.courseCondition}\n\•category: ${body.category}\n\•link: https://learn.dwmaroc.com/courses/${body.slug}\n\Share our channel to support us:\n\@dwmfreecourses`,
-         photo: body.image
-    })
-    return "success"
+        //chat_id: -1001456455807,
+        chat_id: -1002028737966,
+        caption,
+        photo: image,
+    });
+
+    return "success";
 }
 
-
-module.exports = {sendMessage}
+module.exports = { sendMessage };
